8f9cf88dfd4a129b5582714af4b989a9534ff08a
chaeaw <chaeaw04@gmail.com>
2023-07-24 23:08:24 +0900

    [#005 feat] prgressbar 기능 추가
    - setTimeout(functionRef, delay) : functionRef를 delay시간 후에 실
    - 스크롤 전체 영역의 길이를 구하고, 현재 스크롤 된 길이를 구해서 백분율로 계산해준다.
    - throttle : 이벤트 발생 후 특정 시간이 지난 후에 액션 실행(중복 이벤트 방지)
    ㄴ setTimeout을 통해 만들어 사용.

--- case05_progress_bar/practice/script.js
+++ case05_progress_bar/practice/script.js
@@ -1,7 +1,36 @@
 ;(function () {
   'use strict'
 
+  let timerId
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const throttle = (callback, time) => {
+    // 이벤트 발생 후 일정시간 후에 액션이 실행되게 하는 것. setTimeout을 이용.
+    // callback을 time 후에 실행 -> 중복 실행 방지! (과한 클릭 등)
+    if (timerId) return
+
+    timerId = setTimeout(() => {
+      callback()
+      timerId = undefined
+    }, time)
+  }
+
+  const $progressBar = get('.progress-bar')
+
+  const onScroll = () => {
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight
+    // scrollTop이 clientHeight로 인해 scrollHeight의 맨 바닥까지 닿을 수 없으므로
+    // scrollHeight에서 보여지는 부분(clientHeight)는 빼줌
+
+    const scrollTop = document.documentElement.scrollTop
+
+    const width = (scrollTop / height) * 100
+
+    $progressBar.style.width = width + '%'
+  }
+  window.addEventListener('scroll', () => throttle(onScroll, 100))
 })()
